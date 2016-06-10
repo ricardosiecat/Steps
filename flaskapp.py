cb87621b78e0e1a0d58c3b1112e69e0386ed9deb
ricardosiecat <soportetcket@gmail.com>
2016-06-10 01:24:42 -0500

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,14 +1,15 @@
 import os
 from datetime import datetime
-from flask import Flask, request, flash, url_for, redirect, \
-     render_template, abort, send_from_directory
+from flask import g, render_template, flash, redirect, url_for, jsonify, abort
+from flask.ext.login import LoginManager, login_user, logout_user, login_required, current_user
+from flask.ext.qrcode import QRcode
 
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return 'Hey'
 
 @app.route('/<path:resource>')
 def serveStaticResource(resource):
